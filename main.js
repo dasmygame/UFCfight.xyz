2edfba7026817d014a5e1598e05a208d5fa4e826
Rahul Das <48290252+dasmygame@users.noreply.github.com>
2019-06-26 00:10:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,156 @@
+colors = ["blue", "aqua", "teal", "olive", "green", "lime", "yellow", "orange", "red", "fuchsia", "purple", "gray", "silver", "white"];
+
+var u_comments = true;
+
+voted = false;
+
+names = ["FENOMENO", "XIVJan", "Splorgen", "djpocketchange", "Oasis", "Iggypop", "BallsInYourFace", "dopa7", "MasterDragonKing", "ssforfail", "MissyQing", "Endlesss", "badeed", "SmooshyCake", "Karmix", "Alestz", "svbk", "KissMeRDJ", "TeaMALaoSong", "drallaBnayR", "CHRISTHORMANN", "KnivesMillions", "MahNeega", "Sphinx", "Impasse", "Stefono62", "CLGEasy", "GankedFromAbove", "IslandLager", "MrJuneJune", "BrianTheis", "ShorterACE", "morippe", "Meatmush", "Dusey", "Paperkat", "Submit", "TooPro4u", "Porogami", "iuzi", "Suzikai", "TDKNear", "LiquidInori", "Deleted", "NtzLeopard", "UnKooL", "Desu", "Born4this", "sickening", "AllianceMike", "Dinklebergg", "YouGotFaker", "FusionSin", "IMBAYoungGooby", "Neverlike", "BestGodniviaNA", "FFat20GGWP", "kMSeunG", "AliBracamontes", "rua0311desuyo", "54Bomb99", "jivhust", "Penguinpreacher", "Yashimasta", "Erurikku", "ReeferChiefer420", "WonderfulTea", "Gamely", "OberonDark", "Imunne", "Hoeji", "xTearz", "NicoleKidman", "DonDardanoni", "Wonderfuls", "HentaiKatness69", "Ayai", "EREnko", "Cruzerthebruzer", "Connort", "Anoledoran", "BiggestNoob", "Anangelababy007", "TrojanPanda", "MasterCoach", "Kirmora", "wswgou", "NMEotterr", "DragonxCharl", "uJ3lly", "moosebreeder", "Strompest", "Kurumx", "Protective", "LegacyofHao", "DkBnet", "koreas", "AxelAxis", "NiMaTMSiLe", "Preachy", "WoahItsJoe", "XXRhythmMasterXX", "Lemin", "Destinedwithin", "Afflictive", "Nydukon", "Herald0fDeath", "ChowPingPong", "QuanNguyen", "interest", "Slylittlefox121", "VictimOfTalent", "chadiansile", "iToradorable", "BIackWinter", "Mazrer", "NKSoju", "nhocBym", "Dreemo", "Virus", "CowGoesMooooo", "Masrer", "Michaelcreative", "Emanpop", "Druiddroid", "KevonBurt", "Magicians", "HiImYolo", "LoveSick", "kamonika", "Chunkyfresh", "tongsoojosim", "hiimrogue", "Zookerz", "LiShengShun", "DeTFMYumenoti", "EddieMasao", "AGilletteRazor", "andtheknee", "Hazedlol", "SrsBznsBro", "Spreek", "Toxil", "JustinJoe", "Silverblade12345", "WalterWhiteOG", "SwiftyNyce", "Volt", "DoctorElo", "Connie", "DELLZOR", "aiopqwe", "MidnightBoba", "Sikeylol", "Warmogger", "Melhsa", "OmekoMushi", "Life", "SleepyDinosaur", "Leonard", "CatVomit", "Likang45", "PSiloveyou", "xtsetse", "ClydeBotNA", "Cpense", "Arakune", "shadowshifte", "LeeBai", "SexualSavant", "CornChowder", "DeTRFEsteL", "Astro", "deDeezer", "Jayms", "v1anddrotate", "JGLafter", "UhKili", "Aceyy", "Zik", "RiNDiN", "Grandederp", "KawaiiTheo", "Senjogahara", "Th3FooL", "GusTn", "TheTyrant", "GoJeonPa", "DJJingYun", "Egotesticle", "IoveLu", "OGNEunJungCho", "kevybear", "ImJas", "Agrorenn", "Synxia", "DouyuTVForgottt", "GrimSamurai", "6666666666666", "RockleeCtrl", "Xode", "QQ459680082", "KittenAnya", "Zakard", "MARSIRELIA", "WallOfText", "SireSnoopy", "kelppowder", "Hxadecimal", "onelaugh", "MisoMango", "PiggyAzalea", "MisterDon", "VirginEmperor", "suzuXIII", "P18GEMEINV", "Kurumz", "kjin", "CcLiuShicC", "ExileOfTheBlade", "Iambbb", "Fubguns", "Asutarotto", "WhatisLove", "Niqhtmarea", "L0LWal", "JannaFKennedy", "Steffypoo", "KillerHeedonge", "AsianSGpotato", "whiteclaw", "GATOAmyTorin", "lovemyRMB", "Frostarix", "voyyboy", "Melo", "RiotZALE", "ElvishGleeman", "givesyouwiings", "LoveIy", "Packy", "Ntzsmgyu", "Susice", "Dontqqnubz", "mikeshiwuer", "Chulss", "MASTERDING", "Scorpionz", "KKOBONG", "Veeless", "NtzMoon", "Leesinwiches", "RefuseFate", "TP101", "ozoss0", "SeaShell", "Baesed", "Foolish", "jivhust1", "KMadKing", "CHRlSS", "jbraggs", "BeefTacos", "Xoqe", "Naeim", "Aerodactyl", "Triett", "194IQredditor", "Pulzar", "Windgelu", "Suadero", "Zulgor", "Senks", "cAbstracT", "SwagersKing", "AkameBestGirl", "ThePrimaryEdict", "arthasqt", "Lobstery", "MisterOombadu", "TheFriendlyDofu", "Oryziaslatipes", "ugg1", "Flandoor", "HawkStandard", "wimbis", "JimmerFredette", "VikingKarots", "Sorcerawr", "Ciscla", "Suffix", "MrCow", "METALCHOCOB0", "Dessias", "LevelPerfect", "midVox", "Junha", "Hickus", "gamepiong", "AirscendoSona", "HellooKittie", "Jesse", "Rainaa", "ILoveNASoloQ", "Colonelk1", "DeTRFZerost", "Szmao", "TacoKat", "1tzJustVictor", "HomedogPaws", "DioDeSol", "PeterBrown", "FrannyPack", "AbsoluteFridges", "TheBiddler", "ELMdamemitai", "Old", "Pavle", "nathanielbee", "MakiIsuzuSento", "nweHuang", "EvanRL", "yorozu", "forgivenbow", "alexxisss", "Cloverblood", "Entities", "Believe", "Chiruno", "Xiaobanma", "BestJanna", "Neko", "TheEyeofHorus", "IGotSunshine", "Shade20", "Sprusse", "Imacarebear", "Kenleebudouchu", "LockDownExec", "Chubymonkey", "HunterHagen", "Applum", "DaoKho", "MrBlackburn", "beatmymeat", "BestDota2Sona", "chubbiercheeks", "KillaKast", "Betsujin", "TheAmberTeahouse", "BellaFlica", "ManateeWaffles", "Babalew", "charmanderu", "TooSalty", "LotusBoyKiller", "Bulgogeeeee", "Nerzhu1", "Lovelyiris", "QuantumFizzics", "freakingnoodles", "Pdop1", "Bakudanx", "Martel", "DoctorDoom", "equalix", "CARDCAPTORCARD", "Dyad", "Papasmuff", "TheBroskie", "Wadenation", "Flyinpiggy", "Wingsofdeathx", "IamOsiris", "ArtThief", "LotusEdge", "fwii", "Kios", "Shampu", "Nickpappa", "Yukari", "RayXu", "Emeraldancer", "TwoPants", "EnzoIX", "Jacka", "Plumber", "Skadanton", "C9TGleebglarbu", "BonQuish", "GrimmmmmmmReaper", "SmoSmoSmo", "MewtMe", "Ramzlol", "Mruseless", "Eitori", "S0lipsism", "X1337Gm4uLk03rX", "lloveOreo", "MrChivalry", "Oyt", "AnVu", "RBbabbong", "MASTERROSHl", "dabestmelon", "Potatooooooooooo", "KasuganoHaru", "C9BalIs", "stainzoid", "MrArceeSenpaiSir", "sweetinnocence", "Firehazerd", "EpicLynx", "2011", "PandaCoupIe", "Moelon", "KingKenneth", "Skinathonian", "FelixCC", "snowmine", "Acme", "QmoneyAKAQdollas", "Fexir", "ImbaDreaMeR", "ImNovel", "ButtercupShawty", "touch", "penguin", "Promitio", "DeTRFMoyashi", "Hordstyle", "Iizard", "Jintae", "pichumy", "Upu", "Iemonlimesodas", "TwitchTvAuke", "Promises", "Jintea"]
+
+comments = ["nice!", "very good quality", "thanks for streaming", "nice stream", "i hope gustafsson wins", "finally UFC stars", "UFC!!", "JONES KILL HIM", "cant wait ", "love the stream", "i love ufc", "great quality", "finally not lagging", "i love martial arts", "amazing!", "anyone from Poland?", "someone from germany?", "lets go!!",  "whos going for jones?", "lmao my parents wouldn't let me pay for it so here we are", "I had to do the stinking verification twice bc it didn't work the first time", "Verification is asss but it was worth it HA"];
+
+var i = 0;
+
+function deleteComments() {
+    var X = 20
+    if (i > X) {
+        $(".message-" + (i - (X + 1))).remove();
+    }
+}
+
+function randomComment() {
+    var color = colors[Math.floor(Math.random() * colors.length)]
+    var name = names[Math.floor(Math.random() * names.length)]
+    var comment = comments[Math.floor(Math.random() * comments.length)]
+    $(".chat").append('<div class="message message-' + i + '" style="display: none;"><user class="' + color + '">' + name + ':</user> ' + comment + '<hr class="bg-' + color + '"></div>')
+    $(".message-" + i).fadeIn();
+    $(".chat").scrollTop($(".chat")[0].scrollHeight);
+    i += 1;
+    u_comments = false;
+    $(".comments").text(parseInt($(".comments").text()) + 1);
+}
+
+function aComment() {
+    var message = $("#user-message").val();
+    if (message.length > 0) {
+        $("#user-message").removeClass("i-error");
+        $(".chat").append('<div class="message message-' + i + '" style="display: none;"><user class="white">Anonymous:</user> ' + message + '<hr class="bg-white"></div>')
+        $(".message-" + i).fadeIn();
+        $(".chat").scrollTop($(".chat")[0].scrollHeight);
+        $("#user-message").val("");
+        i += 1;
+        u_comments = false;
+        $(".comments").text(parseInt($(".comments").text()) + 1);
+    } else {
+        $("#user-message").addClass("i-error");
+    }
+}
+
+function like() {
+    if (!voted) {
+        $(".likes").text(parseInt($(".likes").text()) + 1);
+        voted = true;
+        $(".fa-thumbs-down").css("opacity", 0.75);
+        $(".dislikes").css("opacity", 0.75);
+        $(".fa-thumbs-down").css("pointer-events", "none");
+        $(".fa-thumbs-up").css("color", "#2ecc40");
+        $(".likes").css("color", "#2ecc40");
+    }
+}
+
+function dislike() {
+    if (!voted) {
+        $(".dislikes").text(parseInt($(".dislikes").text()) + 1);
+        voted = true;
+        $(".fa-thumbs-up").css("opacity", 0.75);
+        $(".likes").css("opacity", 0.75);
+        $(".fa-thumbs-up").css("pointer-events", "none");
+        $(".fa-thumbs-down").css("color", "#ff4136");
+        $(".dislikes").css("color", "#ff4136");
+    }
+}
+
+$("#user-message").keypress(function(e) {
+    if(e.which == 13) {
+        aComment();
+    }
+});
+
+var s_bonus = 0;
+
+var l_bonus = 0;
+
+var d_bonus = 0;
+
+function updateStatistics() {
+    
+    var s_date = new Date();
+    
+    s_bonus += Math.floor(Math.random() * 11) - 5;
+    
+    l_bonus += Math.floor(Math.random() * 4);
+    
+    d_bonus += Math.floor(Math.random() * 2);
+        
+    var online = Math.round((s_date.getSeconds() + (60 * (s_date.getMinutes() + (60 * s_date.getHours())))) / 50 + s_bonus + 2000);
+    
+    var comments = Math.round((s_date.getSeconds() + (60 * (s_date.getMinutes() + (60 * s_date.getHours())))) / 1000 + 100)
+    
+    var likes = Math.round((s_date.getSeconds() + (60 * (s_date.getMinutes() + (60 * s_date.getHours())))) / 250 + l_bonus + 100);
+    
+    var dislikes = Math.round((s_date.getSeconds() + (60 * (s_date.getMinutes() + (60 * s_date.getHours())))) / 1500 + d_bonus + 15);
+    
+    $(".likes").text(likes);
+
+    $(".dislikes").text(dislikes);
+    
+    $(".online").text(online);
+    
+    if (u_comments) {
+        $(".comments").text(comments)
+    }
+    
+}
+
+function play() {
+    
+    $(".s-1").fadeOut("slow", function() {
+        $(".s-2").fadeIn("slow", function() {
+            setTimeout(function() {
+                $(".load").text("Looking for closest server...");
+                setTimeout(function() {
+                    $(".load").text("Connecting...");
+                    setTimeout(function() {
+                        $(".load").html("All servers are full. You need <strong>FREE VERIFY</strong> status to watch the <strong>UFC 232 stream</strong> for free.");
+                        $(".load").addClass("red");
+                        setTimeout(function() {
+                            $(".fa-spin").fadeOut("slow", function() {
+                                $(".verify").fadeIn("slow");
+                            });
+                        }, 3000);
+                    }, 4000);
+                }, 2000);
+            }, 2000);
+        });
+    });
+    
+}
+
+function fixchat() {
+    if ($(window).width() < 768) {
+        $(".chat-column").css("min-height", $(".PLAYER").height())
+        $(".chat-column").css("margin-top", "1rem");
+    };
+;}
+
+$(window).on("resize", function(){
+    fixchat();
+});
+
+fixchat();
+
+updateStatistics();
+randomComment();
+
+setInterval(updateStatistics, 5000);
+setInterval(randomComment, 2500);
+setInterval(deleteComments, 2500);
